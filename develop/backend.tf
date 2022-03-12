5fd61a4e9e83330fa6c3fab9a6b8c133e8c0a026
Shuhei Sasaki <shuheisasaki@ShuheinoMacBook-ea.local>
2022-03-12 19:06:04 +0900

    dev環境用のリソース用意と署名付きCookie利用のための対応

---
+++ develop/backend.tf
@@ -0,0 +1,39 @@
+terraform {
+  backend "s3" {
+    bucket = "sukipi-tfstate"
+    key    = "develop.terraform.tfstate"
+    region = "ap-northeast-1"
+  }
+}
+
+resource "aws_s3_bucket" "tfstate" {
+  bucket = "sukipi-tfstate" 
+
+  lifecycle {
+    prevent_destroy = true
+  }
+}
+
+resource "aws_s3_bucket_acl" "tfstate" {
+    bucket = aws_s3_bucket.tfstate.id
+    acl = "private"
+}
+
+resource "aws_s3_bucket_public_access_block" "tfstate" {
+  bucket = aws_s3_bucket.tfstate.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "tfstate" {
+    bucket = aws_s3_bucket.tfstate.bucket
+
+    rule {
+        apply_server_side_encryption_by_default {
+            sse_algorithm = "AES256"
+        }
+    }
+}
---
+++ develop/variables.tf
@@ -0,0 +1,9 @@
+variable "env" {
+  type    = string
+  default = "develop"
+}
+
+variable "project" {
+  type = string
+  default = "sukipi"
+}
